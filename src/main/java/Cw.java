e1fc7388eed96da9aacee5d5b0cf2c5604d19a9c
ChingWhee <wongchingwhee@gmail.com>
2025-01-27 16:01:55 +0800

    Add "Add, List" task

--- src/main/java/Cw.java
+++ src/main/java/Cw.java
@@ -4,6 +4,8 @@ public class Cw {
     public static void main(String[] args) {
         String name = "Cw";
         Scanner in = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int tasksCount = 0;
 
         System.out.println("Welcome to the Galaxy of " + name + "!");
         Art.printGalaxy();
@@ -13,7 +15,23 @@ public class Cw {
         String line = in.nextLine();
         Art.printDivider();
         while (!line.equalsIgnoreCase("bye")) {
-            System.out.println(line);
+            if (line.isBlank()) {
+                // If user did not type any text, let them know
+                System.out.print("""
+                    Hmm, I can't store invisible thoughts.
+                    Try typing something!
+                    """);
+            } else if (line.equalsIgnoreCase("list")) {
+                // List all user input
+                for (int i = 0; i < tasksCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                // Echo user input
+                System.out.println("added: " + line);
+                tasks[tasksCount] = line;
+                tasksCount++;
+            }
             Art.printDivider();
             line = in.nextLine();
             Art.printDivider();
