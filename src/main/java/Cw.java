03684b596c3f8896ecbc6fef8751043cb2092d44
ChingWhee <wongchingwhee@gmail.com>
2025-01-27 15:28:55 +0800

    Improve greeting message

---
+++ src/main/java/Cw.java
@@ -0,0 +1,11 @@
+public class Cw {
+    public static void main(String[] args) {
+        String name = "Cw";
+
+        System.out.println("Welcome to the Galaxy of Endless Possibilities!");
+        Art.printGalaxy();
+        System.out.println("I am " + name + ". How may I help you?");
+        Art.printDivider();
+    }
+}
+
--- src/main/java/cw.java
+++
@@ -1,8 +0,0 @@
-public class cw {
-    public static void main(String[] args) {
-        String logo = "cw";
-        System.out.println("Hello, I'm " + logo);
-        System.out.println("How may I be of service?");
-        System.out.println("Bye, hope to see you again soon!");
-    }
-}
