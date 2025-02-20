0d290109aa7f46d9f705cc776e9677716e18d7ea
ChingWhee <wongchingwhee@gmail.com>
2025-02-20 15:28:12 +0800

    Use ArrayList to store tasks

--- src/main/java/task/TaskManager.java
+++ src/main/java/task/TaskManager.java
@@ -1,49 +1,47 @@
 package task;
 
 import exception.TaskException;
+import java.util.ArrayList;
 
 public class TaskManager {
-    public static Task[] tasks = new Task[100];
+    public static ArrayList<Task> tasks = new ArrayList<>();
     public static int tasksCount = 0;
 
     public static void changeTaskStatus(Boolean isDone, int index) {
         if (isDone) {
             System.out.println("Nice! I've marked this task as done:");
-            tasks[index - 1].setStatus(true);
-            System.out.println("\t[" + tasks[index - 1].getStatusIcon() + "] "
-                    + tasks[index - 1].getDescription());
+            tasks.get(index - 1).setStatus(true);
+            System.out.println("\t" + tasks.get(index - 1).toString());
         } else {
             System.out.println("OK, I've marked this task as not done yet:");
-            tasks[index - 1].setStatus(false);
-            System.out.println("\t[" + tasks[index - 1].getStatusIcon() + "] "
-                    + tasks[index - 1].getDescription());
+            tasks.get(index - 1).setStatus(false);
+            System.out.println("\t" + tasks.get(index - 1).toString());
         }
     }
 
     public static void printTasks() {
-        if (tasksCount == 0) {
+        if (tasks.isEmpty()) {
             System.out.println("No tasks yet! Start adding some tasks");
             return;
         }
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < tasksCount; i++) {
-            if (tasks[i] != null) {
-                System.out.println((i + 1) + "." + tasks[i].toString());
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i) != null) {
+                System.out.println((i + 1) + "." + tasks.get(i));
             }
         }
     }
 
     public static int getTasksCount() {
-        return tasksCount;
+        return tasks.size();
     }
 
     public static String getLatestTask() {
-        return tasks[tasksCount - 1].toString();
+        return tasks.get(tasks.size() - 1).toString();
     }
 
     public static void addTodo(String description) {
-        tasks[tasksCount] = new Todo(description);
-        tasksCount++;
+        tasks.add(new Todo(description));
     }
 
     public static void addDeadline(String task) throws TaskException {
@@ -51,8 +49,7 @@ public class TaskManager {
             throw new TaskException("Invalid deadline format! Use: deadline <description> /by <time>");
         }
         String[] deadlineParts = task.split(" /by ", 2);
-        tasks[tasksCount] = new Deadline(deadlineParts[0], deadlineParts[1]);
-        tasksCount++;
+        tasks.add(new Deadline(deadlineParts[0], deadlineParts[1]));
     }
 
     public static void addEvent(String task) throws TaskException {
@@ -69,7 +66,8 @@ public class TaskManager {
         if (eventParts[1].contains("/to") || eventParts[2].contains("/from")) {
             throw new TaskException("Invalid order! Ensure /from comes before /to.");
         }
-        tasks[tasksCount] = new Event(eventParts[0], eventParts[1], eventParts[2]);
-        tasksCount++;
+        tasks.add(new Event(eventParts[0], eventParts[1], eventParts[2]));
+    }
+
     }
 }
